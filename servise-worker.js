39894dd5dee5db74f20bd1189e57b22576bc8eec
Novický Tomáš <L20220076@SSPTAJI.CZ>
2024-09-30 10:32:18 +0200

    fixed

--- servise-worker.js
+++ servise-worker.js
@@ -18,7 +18,7 @@ if ('serviceWorker' in navigator) {
           '/',
           '/index.html',
           '/style.css',
-          '/script.js',
+          '/app.js',
           '/icons/icon-192x192.png',
           '/icons/icon-512x512.png'
         ]);
